3d7fbd33e5824d70dfdf3587e09c0ca97c4c1859
David Apakin <apakin.david@gmail.com>
2023-11-03 19:21:32 +0400

    4.final v4

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -3,9 +3,6 @@ package school.mjc.stage0.conditions.finalTask;
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
 
-        System.out.println(first);
-        System.out.println(second);
-
         first ^= second;
         second ^= first;
         first ^= second;
